0961eaa2d87a8f9e9767780eacc79e0a89581c6c
7klow <sysioo.majaro+1@gmail.com>
2025-03-16 12:57:21 +0100

    Update bytecraft.h

--- bytecraft/bytecraft.h
+++ bytecraft/bytecraft.h
@@ -1,37 +1,53 @@
 #ifndef BYTECRAFT_H
 #define BYTECRAFT_H
 
+
 // ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ BINARY ENCODING ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
-void convertBinary  (const char *input, char *output);  // Converts a string to binary
-void decodeBinary   (const char *binary, char *output); // Decodes a binary string into text (ASCII characters)
 
-// USAGE: convertBinary("Hello"     , binaryOutput);
+void str2bin  (const char *input, char *output);  // Converts a string to binary
+void bin2str  (const char *binary, char *output); // Decodes a binary string into text (ASCII characters)
+
+// USAGE: str2bin     ("Hello"     , binaryOutput);
 //                        |> String      |
 //                                       |> String that stores encoded bits
 
-// USAGE: decodeBinary (binaryOutput, decodedOutput);
+// USAGE: bin2str     (binaryOutput, decodedOutput);
 //                       |               |> String that stores decoded text
 //                       |                   
 //                       |> Binary input (String that stores encoded bits)
-
-// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-
-
-
 
 
 // ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ HEXA ENCODING ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
 
-// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+void str2hexa (const char *input, char *output);  // Converts a string to hexadecimal
+void hexa2str (const char *hexa, char *output);   // Decodes a hexadecimal string into text (ASCII characters)
 
+// USAGE: str2hexa    ("Hello"     , hexaOutput);
+//                        |> String      |
+//                                       |> String that stores encoded bits
 
-
+// USAGE: hexa2str    (hexaOutput  , decodedOutput);
+//                       |               |> String that stores decoded text
+//                       |
+//                       |> Hexadecimal input (String that stores encoded bits)
 
 
 // ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ BASE64 ENCODING ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
+
+void str2base64 (const char *base,   char *output); // Converts a string to base64
+void base64tostr(const char *base64, char *output); // Decodes a base64 string into text (ASCII characters)
+
+// USAGE: str2base64  ("Hello"     , base64Output);
+//                        |> String      |
+//                                       |> String that stores encoded bits
+
+// USAGE: base64tostr (base64Output, decodedOutput);
+//                       |               |> String that stores decoded text
+//                       |
+//                       |> Base64 input (String that stores encoded bits)
 
 // ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
